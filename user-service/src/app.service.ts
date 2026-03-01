8723187ae72201a4ea89f091dcae6ea3d1f8eff3
siva prathap konduru <sivaprathap.konduru@gmail.com>
2026-03-01 15:25:30 +0530

    fix: trying to build the version bumping

--- user-service/src/app.service.ts
+++ user-service/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! will be fix in next commit';
+    return 'Hello World!';
   }
 }
